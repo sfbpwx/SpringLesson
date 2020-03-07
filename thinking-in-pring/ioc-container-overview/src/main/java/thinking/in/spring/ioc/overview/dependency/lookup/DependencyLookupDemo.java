12d83f15b93fb8464332dddba05d5d81932bba1a
wangqian <124008217@qq.com>
2020-03-08 00:34:20 +0800

    spring-bean相关内容

---
+++ thinking-in-pring/ioc-container-overview/src/main/java/thinking/in/spring/ioc/overview/dependency/lookup/DependencyLookupDemo.java
@@ -0,0 +1,11 @@
+package thinking.in.spring.ioc.overview.dependency.lookup;
+
+import org.springframework.bean.factory.Beanfactory;
+/**
+ * 依赖查找样例
+ */
+public class DependencyLookupDemo {
+    public static void  main(String args[]){
+        BeanFactory beanFactory = new Classpath
+    }
+}
---
+++ thinking-in-pring/ioc-container-overview/src/main/java/thinking/in/spring/ioc/overview/domain/User.java
@@ -0,0 +1,22 @@
+package thinking.in.spring.ioc.overview.domain;
+
+public class User {
+    private long id;
+    private String name1;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName1() {
+        return name1;
+    }
+
+    public void setName1(String name1) {
+        this.name1 = name1;
+    }
+}
