9fde77700e95d20b30e7963650cb6211eaf9f259
wangqian <124008217@qq.com>
2020-03-25 14:25:01 +0800

    IOC查找bean方式，去掉多余方法 增加注释

--- thinking-in-spirng/ioc-container-overview/src/main/java/thinking/in/spring/ioc/overview/dependency/lookup/DependencyLookupDemo.java
+++ thinking-in-spirng/ioc-container-overview/src/main/java/thinking/in/spring/ioc/overview/dependency/lookup/DependencyLookupDemo.java
@@ -1,7 +1,6 @@
 package thinking.in.spring.ioc.overview.dependency.lookup;
 
 import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.FactoryBean;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -21,34 +20,50 @@ public class DependencyLookupDemo {
         //启动应用上下文
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("META-INF/dependency-look-up-context.xml");
 //        //实时查找
-//        lookupInrealTime(beanFactory);
+        lookupInrealTime(beanFactory);
 //        //延时查找
-//        lookupInLazy(beanFactory);
+        lookupInLazy(beanFactory);
         //按照类型查找
-//        lookupByType(beanFactory);
+        lookupByType(beanFactory);
         //按照集合对象类型查找
-//        lookupByCollectionByType(beanFactory);
+        lookupByCollectionByType(beanFactory);
         //通过注解方式查找
         lookupByAnnotationType(beanFactory);
     }
 
+    /**
+     * 实时查询
+     * @param beanFactory
+     */
     private static void lookupInrealTime(BeanFactory beanFactory){
         //老版本强转型
         User user = (User) beanFactory.getBean("user");
         System.out.println("实时查找"+user.toString());
     }
 
+    /**
+     * 非实时查询
+     * @param beanFactory
+     */
     private  static void lookupInLazy(BeanFactory beanFactory){
         ObjectFactory<User> objectFactory = (ObjectFactory)beanFactory.getBean("objectFactory");
         User user = objectFactory.getObject();
         System.out.println("延迟查找:"+user);
     }
 
+    /**
+     * 按照类型查询
+     * @param beanFactory
+     */
     private static void lookupByType(BeanFactory beanFactory){
         User user = beanFactory.getBean(User.class);
         System.out.println("实时查找"+user.toString());
     }
 
+    /**
+     * 按照几何分类查询
+     * @param beanFactory
+     */
     private static void lookupByCollectionByType(BeanFactory beanFactory){
         if(beanFactory instanceof ListableBeanFactory){
             ListableBeanFactory listableBeanFactory = (ListableBeanFactory)beanFactory;
@@ -57,6 +72,10 @@ public class DependencyLookupDemo {
         }
     }
 
+    /**
+     * 按照注解方式查询
+     * @param beanFactory
+     */
     private static void lookupByAnnotationType(BeanFactory beanFactory){
         if(beanFactory instanceof ListableBeanFactory){
             ListableBeanFactory listableBeanFactory = (ListableBeanFactory)beanFactory;
